1c2ed743b9b413b1653bc1d6d6bfc5a4a323464b
Shaun Price <shaundnz@gmail.com>
2019-01-02 20:54:42 +1300

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -20,5 +21,6 @@ gulp.task('coffee', function () {
 gulp.task('js', function () {
  gulp.src(jsSources)
      .pipe(concat('script.js'))
+     .pipe(browserify())
      .pipe(gulp.dest('builds/development/js'))
 });
